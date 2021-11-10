c3def402955b9602b6ab780681e8c18f9b31c9e7
Albert <albert.bagil@gmail.com>
2021-11-10 18:18:00 +0700

    Tugas 2 PHP
    Tugas 2 PHP untuk dikumpulkan ke Niomic

---
+++ Tugas2_1.php
@@ -0,0 +1,11 @@
+<?php
+$hari = 36;
+$pekerja1 =12;
+$pekerja2 =24;
+$hasil = ($hari * $pekerja1)/$pekerja2;
+
+echo "$hari Hari = $pekerja1 Pekerja <br>";
+echo "X hari = $pekerja2 Pekerja <br>";
+echo "X = ...? <br>";
+echo "Jawaban : $hasil";
+ ?>
